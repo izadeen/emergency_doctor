bc15cd14016b91c26d51debfffa4b42460cdf23e
izadeen <izadeenalkoran@gmail.com>
2022-05-19 07:56:41 +0300

    init

---
+++ emergency_doctor/src/main/java/com/example/emergency_doctor/EmergencyDoctorApplication.java
@@ -0,0 +1,13 @@
+package com.example.emergency_doctor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmergencyDoctorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmergencyDoctorApplication.class, args);
+	}
+
+}
---
+++ emergency_doctor/src/test/java/com/example/emergency_doctor/EmergencyDoctorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.emergency_doctor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmergencyDoctorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
